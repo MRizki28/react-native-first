040077df63c84f1453b03c29f5b1b72c733b4ad4
MRizki28 <muhammadrizkitkj123@gmail.com>
2023-09-06 18:36:24 +0800

    first

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native'
+import React, { Component } from 'react'
+import Body from './src/component/Body'
+
+export default class App extends Component {
+  render() {
+    return (
+    <Body></Body>
+    )
+  }
+}
---
+++ src/component/Body.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Body = () => {
+  return (
+    <View>
+      <Text>Halo saya muhammad Rizki</Text>
+    </View>
+  )
+}
+
+export default Body

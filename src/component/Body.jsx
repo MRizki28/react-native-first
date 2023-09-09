91f3e66b6bde779c60735b1d411c86cc4914608a
MRizki28 <muhammadrizkitkj123@gmail.com>
2023-09-09 20:10:48 +0800

    update day 2

--- src/component/Body.jsx
+++ src/component/Body.jsx
@@ -1,12 +1,42 @@
-import { View, Text } from 'react-native'
+import { View, Text, ScrollView, StyleSheet, TextInput } from 'react-native'
 import React from 'react'
+import { Image } from 'react-native-ui-lib'
 
 const Body = () => {
   return (
-    <View>
-      <Text>Halo saya muhammad Rizki</Text>
-    </View>
-  )
-}
+    <ScrollView>
+      <Text style={{ textAlign: 'center' }}>Header</Text>
+      <View style={styles.container}>
+        <Image
+          source={{ uri: 'https://reactnative.dev/docs/assets/p_cat2.png' }}
+          style={styles.image}
+        />
+      </View>
+      <ScrollView>
+        <TextInput style={styles.textInput}>
+        </TextInput>
+      </ScrollView>
+    </ScrollView>
+  );
+
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center', // Mengatur penempatan vertikal menjadi tengah
+    alignItems: 'center', // Mengatur penempatan horizontal menjadi tengah
+  },
+  image: {
+    width: 200,
+    height: 200,
+    // Tambahkan properti gaya untuk gambar di sini
+  },
+  textInput: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+  }
+});
 
 export default Body
